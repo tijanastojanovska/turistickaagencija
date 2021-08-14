b06b3ee03a3457ba0d1cbf682f824c00907dd44a
Miroslav Radic <radicmiroslav101@gmail.com>
2021-08-14 16:46:04 +0200

    mvc mock tests

---
+++ src/test/java/com/example/turistickaagencija/AdresaServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.turistickaagencija;
+
+import com.example.turistickaagencija.service.AdresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+public class AdresaServiceTest {
+
+    @Autowired
+    AdresaService adresaService;
+
+    @Test
+    public void test1() {
+
+    }
+}
