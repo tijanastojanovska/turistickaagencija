1bbebca4b8b3bca5e0f93d38eb2fc465f776350a
darko <darkoskerlevski@gmail.com>
2021-08-21 22:12:03 +0200

    jasmine implementation

---
+++ src/main/resources/jasmine/spec/mapSpec.js
@@ -0,0 +1,8 @@
+describe("Checking the lon and lat values", function() {
+
+    it("Expecting returns", function() {
+
+        expect(lon).toEqual(21.398902300000003);
+        expect(lat).toEqual(42.00311530048647);
+    });
+})
